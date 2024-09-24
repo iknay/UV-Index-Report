d7dbd32998cda515c9d32a08b8311825cd7005d3
iknay <yanceevillanueva16@gmail.com>
2024-09-24 16:57:59 +0800

    m

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import express from "express";
 import axios from "axios";
 import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const port = process.env.PORT || 3000;
